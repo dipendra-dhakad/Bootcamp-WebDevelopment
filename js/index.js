e053d88dc4f3db1e244d94ec984ee330296c08c0
Dipendra Dhakad <dhakaddipendradhakad@gmail.com>
2023-10-27 15:42:19 +0530

    web development

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// let myDiv =document.createElement('div');
+// function paraStatus(event){
+//     console.log('I have clicked on para');
+// }
+
+// myDiv.addEventListener('click' , paraStatus);
+// for(let i=1;i<=100;i++){
+//     let newElement =document.createElement('p');
+//     newElement.textContent ='This is para' +i;
+//     myDiv.appendChild(newElement);
+// }
+// document.body.appendChild(myDiv);
+
+//https://github.com/dipendra-dhakad/Bootcamp-WebDevelopment.git
+
+let element =document.querySelector('#wrapper');
+
+element.addEventListener('click',function(event){
+    if (event.target.nodeName === 'SPAN') {
+     console.log('span pr click kia hai' + event.target.textContent);
+    }
+});  
